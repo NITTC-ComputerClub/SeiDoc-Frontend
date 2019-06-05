6e20d288196560253395ca7ff88a9462045b62be
笹本康太 <sasamotokouta@sasamotokoutanoMacBook-Pro.local>
2019-06-06 00:47:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/friends'>Friends</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/friends' component={Friends} />
+        </div>
+    </BrowserRouter>
+    );
+  }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+)
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+)
 
 export default App;
