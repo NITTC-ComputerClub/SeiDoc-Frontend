3cd22ea915365ee8f76a97fed461bf028891a83c
笹本康太 <sasamotokouta@sasamotokoutanoMacBook-Pro.local>
2019-06-10 15:29:32 +0900

    [add]検索画面

---
+++ src/components/search.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import axios from "axios";
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            value: '',
+            message: ''
+        };
+        this.handle_requests = this.handle_requests.bind(this)
+    }
+
+    handle_requests = () => {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        axios
+            .get('https://golang-with-dbserver.herokuapp.com/ping', headers)
+            .then(
+                (results) => {
+                    this.setState({
+                        loading: true,
+                        itmes: results.data
+                    })
+                    console.log(this.state.itmes)
+                },
+                (error) => {
+                    console.log(error)
+                })
+    }
+
+    handleInput = ({ target: { value } }) => {
+        this.setState({
+            value
+        });
+    }
+
+    send = () => {
+        const { value } = this.state;
+        console.log(value)
+        this.setState({
+            value: '',
+            message: value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)} />
+                <button onClick={this.send.bind(this)}>SEND</button>
+                <h1>カテゴリ</h1>
+                <ul>
+                    <li>
+                        <Link to='/api'>介護</Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Search;
