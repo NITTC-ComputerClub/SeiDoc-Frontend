4abb61ef9475e14966da1c5f6a752cf619d6b393
笹本康太 <sasamotokouta@sasamotokoutanoMacBook-Pro.local>
2019-06-11 12:36:23 +0900

    [fix] category

--- src/components/category.js
+++ src/components/category.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 
 class Category extends Component {
     constructor(props) {
