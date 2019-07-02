9b293859f6d2ae2623b653b78d5b6bb35a6d840b
sasa-kou <sasakou11428@gmail.com>
2019-07-02 15:21:28 +0900

    add ロード画面

--- src/components/indicator.js
+++ src/components/indicator.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 
 class Indicator extends React.Component {
     render() {
         return (
             <div>
-                <script src="https://kit.fontawesome.com/5b617c9d7c.js"></script>
-                {/*<i className="fa fa-refresh fa-spin fa-5x"></i>*/}
-                <i className="fas fa-tasks fa-lg"></i>
+                <FontAwesomeIcon icon={faCircleNotch} spin size="6x" />
             </div>
         )
     }
