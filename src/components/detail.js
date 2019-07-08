7ca287886cd83552227d1b126eac4c39a3c04359
sasa-kou <sasakou11428@gmail.com>
2019-07-08 17:23:12 +0900

    hot-fix detail.js targetAge->Target

--- src/components/detail.js
+++ src/components/detail.js
@@ -11,7 +11,7 @@ class Detail extends Component {
                 <h2>担当部署</h2>
                 <p>{this.props.detail.departmentInCharge}</p>
                 <h2>おまかな制度対象者</h2>
-                <p>{this.props.detail.targetAge}歳</p>
+                <p>{this.props.detail.Target}</p>
                 <h2>内容</h2>
                 <p id="systemDescription">{this.props.detail.detail}</p>
                 <a target="_blank" rel="noopener noreferrer" href={this.props.detail.site}>
