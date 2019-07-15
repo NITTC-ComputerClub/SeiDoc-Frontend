acc78a540eab4c10203bf965fd771ad99410dbec
sasa-kou <sasakou11428@gmail.com>
2019-07-16 01:01:21 +0900

    fix redux-thunk

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,16 +1,46 @@
 import actionCreatorFactory from 'typescript-fsa'
+import { Action } from 'typescript-fsa'
+import { Dispatch } from "redux"
+import { SystemState } from '../reducers/systemReducer'
+
+export interface System {
+    Name: string,
+    Department: string,
+    Location: string,
+    Site: string,
+    Detail: string,
+    Target: string,
+    Method: Array<string>,
+    Category: Array<string>
+}
 
 const actionCreator = actionCreatorFactory()
 
 export const systemFetch = {
-    setName: actionCreator<string>('SET_NAME'),
-    setDepartment: actionCreator<string>('SET_DEPARTMENT'),
-    setLocation: actionCreator<string>('SET_LOCATION'),
-    setSite: actionCreator<string>('SET_SITE'),
-    setDetail: actionCreator<string>('SET_DETAIL'),
-    setTarget: actionCreator<string>('SET_TARGET'),
-    setMethod: actionCreator<Array<string>>('SET_METHOD'),
-    setCategory: actionCreator<Array<string>>('SET_CATEGORY')
+    loadSystem: actionCreator.async<{}, System[]>('LOAD_SYSTEM')
+}
+export function loadAllSystem() {
+    return (dispatch: Dispatch<Action<any>>, getState: () => SystemState<System>) => {
+        return new Promise<System>((resolve, reject) => {
+            dispatch(systemFetch.loadSystem.started({ params: {} }))
+            fetch('https://script.googleusercontent.com/macros/echo?user_content_key=IoHQsv9bVzmPxlj0JrzpZu8-bMOFCP-aOy6MagzjbqTUjX7ZWnvNuDYtpaRKNzRrkY5Uf0t7cazxBMPH0KJb4np7J8BCtmwXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMzktTexFYrRIaapUjyBLsE5tL0B1WpvsST2ScRPWj3LGn1eoFquhGv_FmCDt-laYD9DOGu144H9&lib=MiOBqfRgCOdo7KPNv4oTDDfMEFKE191aw')
+                .then(res => {
+                    return res.json()
+                        .then(json => {
+                            if (res.ok) {
+                                dispatch(systemFetch.loadSystem.done({ result: json, params: {} }))
+                                resolve(json)
+                            } else {
+                                dispatch(systemFetch.loadSystem.failed({ error: json, params: {} }))
+                                reject(new Error("..."))
+                            }
+                        })
+                }).catch(error => {
+                    dispatch(systemFetch.loadSystem.failed({ error: error, params: {} }))
+                    reject(error)
+                })
+        })
+    }
 }
 
 export const addTags = actionCreator<string>('ADD_TAGS')
--- src/components/categoryButton.tsx
+++ src/components/categoryButton.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
+import { State } from '../reducers/systemReducer'
+import { SystemActions } from '../containers/systemContainer'
 
-const CategoryButton: React.SFC = () => {
+type CH = State & SystemActions
+const CategoryButton: React.SFC<CH> = (props: CH) => {
     return (
         <div>
             <div>
-                <button>カテゴリー</button>
+                <p>{props.all.loading}</p>
+                <p>{props.all.error}</p>
+                <button onClick={props.loadAllSystem}>カテゴリー</button>
+                {console.log(props)}
             </div>
         </div>
     )
