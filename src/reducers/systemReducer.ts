4ba9e5f8c01a536f2cd60c18ef0203830860be11
sasa-kou <sasakou11428@gmail.com>
2019-07-15 22:49:49 +0900

    add systemReducer

---
+++ src/reducers/systemReducer.ts
@@ -0,0 +1,29 @@
+import { reducerWithInitialState } from 'typescript-fsa-reducers'
+import { systemFetch } from '../actions/action'
+
+export interface SystemState {
+    Name: string,
+    Department: string,
+    Location: string,
+    Site: string,
+    Detail: string,
+    Target: string,
+    Method: Array<string>,
+    Category: Array<string>
+}
+
+const initialState: SystemState = {
+    Name: '',
+    Department: '',
+    Location: '',
+    Site: '',
+    Detail: '',
+    Target: '',
+    Method: [],
+    Category: []
+}
+
+export const SystemReducer = reducerWithInitialState(initialState)
+    .case(systemFetch.setName, (state) => {
+        
+    })
