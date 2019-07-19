a2342f8185e9b95086939dc7b65206c3b6c1ae95
sasa-kou <sasakou11428@gmail.com>
2019-07-20 01:09:41 +0900

    新しい技術は積極的に使っていこう

--- src/containers/systemListContainer.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import { AppState } from '../store'
-import SystemList from '../components/systemList'
-
-function mapStateToProps(appState: AppState){
-    return appState.systems
-}
-
-export default connect(mapStateToProps)(SystemList)
