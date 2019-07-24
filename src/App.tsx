ad82bd55425fb691e4cbe63805f9df35f3afca74
Hitoki Minami <mkmokymh@gmail.com>
2019-07-25 02:29:31 +0900

    add: 色とフォントサイズ設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Category from './atoms/category'
 import Result from './atoms/result'
 import Input from './atoms/input'
 import Detail from './atoms/detail'
+import './App.scss'
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="container">
           <Route exact path='/' component={Category} />
           <Route path='/result' component={Result} />
           <Route path='/input' component={Input} />
