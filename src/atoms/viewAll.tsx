ecf95df8e8f52fdd5113f385ab4f9092a2c43135
sasa-kou <sasakou11428@gmail.com>
2019-07-24 13:42:45 +0900

    add getAllData

--- src/atoms/viewAll.tsx
+++ src/atoms/viewAll.tsx
@@ -1,8 +1,21 @@
 import React, { useEffect } from 'react';
+import { fireStore } from '../firebase/index'
 import { System } from '../reducers/systemsReducer'
 
 const ViewAll: React.FC = () => {
-    const fetchSystemAll = () => {}
+    const fetchSystemAll = () => {
+        const systemsData: Array<System> = []
+        fireStore.collection('systems').get()
+            .then(
+                (snapshot) => {
+                    snapshot.forEach((doc) => {
+                        systemsData.push(doc.data() as System)
+                    })
+                }
+            ).then(() => {
+                console.log(systemsData)
+            })
+    }
     const updateSystem = (uuid: string, newData: System) => { }
     const deleteSystem = (uuid: string) => { }
 
@@ -12,6 +25,7 @@ const ViewAll: React.FC = () => {
     return (
         <div>
             <h2>全データ</h2>
+            <button onClick={fetchSystemAll}>取得</button>
             <button>追加</button>
             <button>削除</button>
         </div>
