51be8399623825df3c0afdd0733ff51f835a8b7b
Takamichi Omori <merrytakamerry@gmail.com>
2019-07-24 20:44:49 +0900

    一通りひょうじはできた

--- src/atoms/viewAll.tsx
+++ src/atoms/viewAll.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { fireStore } from '../firebase/index'
 import { System } from '../reducers/systemsReducer'
 import systemList from '../components/systemList';
+import { faDivide } from '@fortawesome/free-solid-svg-icons';
 
 type systemData = {
     id: string,
@@ -14,6 +15,7 @@ type systemData = {
 const ViewAll: React.FC = () => {
     let [searchData, setSearchData] = useState<{ [key: string]: systemData }>({})
     const [originalData, setOriginalData] = useState<{ [key: string]: systemData }>({})
+    const [isFetched, setFetchFlag] = useState<Boolean>(false)
 
     const fetchSystemAll = () => {
         const dataList: { [key: string]: systemData } = {};
@@ -28,6 +30,7 @@ const ViewAll: React.FC = () => {
             ).then(() => {
                 setSearchData(dataList)
                 setOriginalData(dataList)
+                setFetchFlag(!isFetched)
                 console.log(dataList)
             })
     }
@@ -40,17 +43,19 @@ const ViewAll: React.FC = () => {
     }, [])
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
-        searchData[key].data.Name = e.target.value;
+        let tmp  = e.target.name as 'Name' | 'Site' //TODO: 黒魔術やめる
+        searchData[key].data[tmp] = e.target.value;
         //あたらしいStateをset
-        setSearchData(Object.assign({}, searchData, {
-            key: {
-                data: {
-                    Name: e.target.value
-                }
-            }
-        }));
+        setSearchData(Object.assign({}, searchData)); 
         console.log(key)
         console.log(searchData[key].data.Name)
+    }
+
+    const handleDeleteCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>,key: string) => {
+        searchData[key].willDelete = e.target.checked
+        setSearchData(Object.assign({}, searchData));
+        console.log(e.target.checked)
+
     }
 
     return (
@@ -59,14 +64,17 @@ const ViewAll: React.FC = () => {
             <button onClick={fetchSystemAll}>取得</button>
             <button>追加</button>
             <button>削除</button>
-            <table>
+            
+            {isFetched?<table>
                 <thead>
                     <tr>
+                        <th>削除</th>
                         <th>制度名</th>
                         <th>課</th>
                         <th>場所</th>
                         <th>URL</th>
                         <th>詳細</th>
+                        <th>対象</th>
                         <th>方法</th>
                         <th>カテゴリ</th>
                     </tr>
@@ -75,20 +83,20 @@ const ViewAll: React.FC = () => {
                     {
                         Object.keys(searchData).map(key =>
                             <tr key={key}>
-                                <td><input type='checkbox' value={searchData[key].id}></input></td>
-                                {/* 名前のとこ変更したい */}
-                                <td><input type='text' name='name' value={searchData[key].data.Name} onChange={e => handleInputChange(e, key)}></input></td>
-                                <td>{searchData[key].data.Department}</td>
-                                <td>{searchData[key].data.Location}</td>
-                                <td>{searchData[key].data.Site}</td>
-                                <td>{searchData[key].data.Detail}</td>
-                                <td>{searchData[key].data.Target}</td>
-                                <td>{searchData[key].data.Method}</td>
-                                <td>{searchData[key].data.Category}</td>
+                                <td><input type='checkbox' value={searchData[key].id} onChange={e => handleDeleteCheckboxChange(e,key)}></input></td>
+                                <td><input type='text' name='Name' value={searchData[key].data.Name} onChange={e => handleInputChange(e, key)}></input></td>
+                                <td><input type='text' name='Department' value={searchData[key].data.Department} onChange={e => handleInputChange(e, key)}></input></td>
+                                <td><input type='text' name='Location' value={searchData[key].data.Location} onChange={e => handleInputChange(e,key)}></input></td>
+                                <td><input type='text' name='Site' value={searchData[key].data.Site} onChange={e => handleInputChange(e,key)}></input></td>
+                                <td><input type="text" name="Detail" value={searchData[key].data.Detail} onChange={e => handleInputChange(e,key)}></input></td>
+                                <td><input type="text" name="Target" value={searchData[key].data.Target} onChange={e => handleInputChange(e,key)}></input></td>
+                                <td><input type="text" name="Method" value={searchData[key].data.Method} onChange={e => handleInputChange(e,key)}></input></td>
+                                <td><input type="text" name="Category" value={searchData[key].data.Category} onChange={e => handleInputChange(e,key)}></input></td>
                             </tr>)
                     }
                 </tbody>
             </table>
+            : <div></div>}
         </div>
     )
 }
