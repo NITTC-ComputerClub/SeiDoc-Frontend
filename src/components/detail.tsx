02974c5f91d759bd0f0b22e80149ccadbc00abdd
sasa-kou <sasakou11428@gmail.com>
2019-07-24 10:21:25 +0900

    add view

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -4,12 +4,22 @@ import { AppState } from '../store'
 
 
 const Detail: React.FC = () => {
-    const systemList = useSelector((state: AppState) => state.selectsystemState.selectSystem)
-    console.log(systemList)    
+    const detail = useSelector((state: AppState) => state.selectsystemState.selectSystem)
+    console.log('detail', detail)
     return (
         <div>
-            <div>
-            </div>
+            <h1>{detail.Name}</h1>
+            <h2>援助対象者</h2>
+            <p>{detail.Target}</p>
+            <h2>援助方法</h2>
+            <p>{detail.Method}</p>
+            <h2>担当部署</h2>
+            <p>{detail.Department}</p>
+            <h2>詳細</h2>
+            <p>{detail.Detail}</p>
+            <a target="_blank" rel="noopener noreferrer" href={detail.Site}>
+                <button>公式のページへ</button>
+            </a>
         </div>
     )
 }
