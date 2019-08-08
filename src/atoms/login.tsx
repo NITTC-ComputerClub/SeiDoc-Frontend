94311092a1fa8b5dba344670ee02a0b083d64bfa
Takamichi Omori <merrytakamerry@gmail.com>
2019-08-09 01:22:39 +0900

    データ登録

--- src/atoms/login.tsx
+++ src/atoms/login.tsx
@@ -1,13 +1,21 @@
 import React, {useState} from 'react'
-import  { auth } from '../firebase/firebase'
+import  { fireStore, auth } from '../firebase/firebase'
 import firebase from 'firebase'
 
 type loginData = {
     email: string,
     password: string
 }
+type userData = {
+    birthday: string,
+    income: number,
+    address: string,
+    family: string
+}
+
 const Login: React.FC = () => {
     let [loginData, setLoginData] = useState<loginData>({email: '',password:''})
+    let [userData, setUserData] = useState<userData>({birthday: '',income: 0, address: '',family: ''})
 
     const handleSignUp = () => {
         const email = loginData.email
@@ -64,6 +72,19 @@ const Login: React.FC = () => {
         setLoginData(Object.assign({}, loginData))
     }
 
+    const handleUserdataInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name as 'birthday' |  'address' | 'family' 
+        userData[name] = e.target.value
+        setUserData(Object.assign({}, userData))
+    }
+
+    const handleSetUserdata = () => {
+        const user = auth.currentUser as firebase.User
+        fireStore.collection('user').doc(user.uid).set(userData).then(res => {
+            console.log(user.uid)
+        })
+    }
+
     return (
         <div>
         {!auth.currentUser?
@@ -82,6 +103,16 @@ const Login: React.FC = () => {
             <div>
                 <div>ようこそ、{auth.currentUser.uid}</div>
                 <input type="submit" name="submit" value="SignOut" onClick={()=>handleSignOut()}></input>
+
+                <div>生年月日</div>
+                    <input type="date" name="birthday" value={userData.birthday} onChange={e => handleUserdataInputChange(e)}></input>
+                <div>年収</div>
+                    <input type="number" name="income" value={userData.income} onChange={e => handleUserdataInputChange(e)}></input>
+                <div>居住区</div>
+                    <input type="text" name="address" value={userData.address} onChange={e => handleUserdataInputChange(e)}></input>
+                <div>家族構成</div>
+                    <input type="text" name="family" value={userData.family} onChange={e => handleUserdataInputChange(e)}></input>
+                <input type="submit" name="submit" value="submit" onClick={() => handleSetUserdata()}></input>
             </div>
         }   
         </div>
