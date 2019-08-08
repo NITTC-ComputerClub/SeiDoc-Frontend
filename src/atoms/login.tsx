f64baf66b2bb8cfe628ab7993c24011742500521
Takamichi Omori <merrytakamerry@gmail.com>
2019-08-08 23:45:27 +0900

    フォームが出来た

--- src/atoms/login.tsx
+++ src/atoms/login.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+type loginData = {
+    email: string,
+    password: string
+}
 const Login: React.FC = () => {
+    let [loginData, setLoginData] = useState<loginData>({email: '',password:''})
+
+    const onClickSubmit = () => {
+        console.log(loginData.email)
+        console.log(loginData.password)
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //console.log(e.target.name)
+        const name = e.target.name as 'email' | 'password'
+        loginData[name] = e.target.value
+        setLoginData(Object.assign({}, loginData))
+    }
+
     return (
         <div>
             <div>Login</div>
-            <input type="text" name="email"></input>
-            <input type="text" name="password"></input>
-            <input type="submit" name="submit" value="submit"></input>
+            <input type="text" name="email" value={loginData.email} onChange={e => handleInputChange(e)}></input>
+            <input type="text" name="password" value={loginData.password} onChange={e => handleInputChange(e)}></input>
+            <input type="submit" name="submit" value="submit" onClick={()=>onClickSubmit()}></input>
         </div>
 
     )
