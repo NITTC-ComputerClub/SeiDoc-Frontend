e13b15e4b6ff8a892a9a1262f9d8ea233d99bb69
Takamichi Omori <merrytakamerry@gmail.com>
2019-08-09 00:05:45 +0900

    新規登録できた

--- src/atoms/login.tsx
+++ src/atoms/login.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { auth } from '../firebase/firebase'
 
 type loginData = {
     email: string,
@@ -7,13 +8,36 @@ type loginData = {
 const Login: React.FC = () => {
     let [loginData, setLoginData] = useState<loginData>({email: '',password:''})
 
-    const onClickSubmit = () => {
-        console.log(loginData.email)
-        console.log(loginData.password)
+    const handleSignUp = () => {
+        const email = loginData.email
+        const password = loginData.password
+        console.log(email)
+        console.log(password)
+        
+        if (password.length < 8) {
+            alert('Please enter a password')
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).then(res => {
+            console.log(res)
+        }).catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            if(errorCode === 'auth/weak-password') {
+                alert('The password is too weak.')
+            } else {
+                alert(errorMessage)
+            }
+            console.log(error)
+        })
+    }
+
+    const handleSignIn = () => {
+        console.log('Sign in!')
     }
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        //console.log(e.target.name)
         const name = e.target.name as 'email' | 'password'
         loginData[name] = e.target.value
         setLoginData(Object.assign({}, loginData))
@@ -21,10 +45,16 @@ const Login: React.FC = () => {
 
     return (
         <div>
-            <div>Login</div>
+            <div>新規登録</div>
             <input type="text" name="email" value={loginData.email} onChange={e => handleInputChange(e)}></input>
             <input type="text" name="password" value={loginData.password} onChange={e => handleInputChange(e)}></input>
-            <input type="submit" name="submit" value="submit" onClick={()=>onClickSubmit()}></input>
+            <input type="submit" name="submit" value="submit" onClick={()=>handleSignUp()}></input>
+            <br></br>
+            <div>ログイン</div>
+            <input type="text" name="email" value={loginData.email} onChange={e => handleInputChange(e)}></input>
+            <input type="text" name="password" value={loginData.password} onChange={e => handleInputChange(e)}></input>
+            <input type="submit" name="submit" value="submit" onClick={()=>handleSignIn()}></input>
+        
         </div>
 
     )
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,3 +4,4 @@ import firebaseConfig from './config'
 firebase.initializeApp(firebaseConfig)
 
 export const fireStore = firebase.firestore()
+export const auth = firebase.auth()
