512608e16b57f26560bda65b3b7663bec6ab4d4f
Hitoki Minami <mkmokymh@gmail.com>
2019-08-12 18:56:45 +0900

    add: topページタイトル部分

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -2,13 +2,21 @@ import * as React from 'react'
 import SearchButton from '../components/Top/searchButton'
 import PopularSystemCard from '../components/Top/popularSystemCard'
 import Ranking from '../components/Top/ranking'
+import '../scss/top.scss'
 
 const Top: React.FC = () => {
     return (
-        <div>
-            <SearchButton buttonName="制度名から調べる" nextLocation="/category"/>
-            {/* TODO: 地域から調べるページへ遷移 */}
-            <SearchButton buttonName="地域から調べる" nextLocation="/"/>
+        <div className="top">
+            <div className="title">
+                <div className="wrapper">
+                    <img src="img/logo.png" alt="SeiDocロゴ"></img>
+                </div>
+                <div className="buttons">
+                    <SearchButton buttonName="制度名から調べる" nextLocation="/category" buttonColor="#44DD9D" />
+                    {/* TODO: 地域から調べるページへ遷移 */}
+                    <SearchButton buttonName="地域から調べる" nextLocation="/" buttonColor="#449DDD" />
+                </div>
+            </div>
             <h2>みんなが見ている制度</h2>
             <PopularSystemCard />
             <h2>住みやすい街ランキング</h2>
