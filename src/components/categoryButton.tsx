e85f8d96c9bb59b8563ba745f7c6ad67a52c73eb
sasa-kou <sasakou11428@gmail.com>
2019-08-13 18:26:57 +0900

    add クエリストリングを用いた遷移とfetch

--- src/components/categoryButton.tsx
+++ src/components/categoryButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { fetchSystemByCategory, addTagCreator } from '../actions/action'
+import { addTagCreator } from '../actions/action'
 import { withRouter, RouteComponentProps } from 'react-router'
 import '../scss/categoryButton.scss'
 
@@ -8,7 +8,6 @@ type historyProps = RouteComponentProps
 
 const CategoryButton: React.FC<historyProps> = (props) => {
     const dispatch = useDispatch()
-    const categorySearch = (category: string) => dispatch(fetchSystemByCategory(category))
     const addTag = (newTag: string) => dispatch(addTagCreator(newTag))
     const categoryList: Array<string> = [
         '子育て', '介護', '建築', '病気', '融資', '地域', '高齢者'
@@ -18,13 +17,12 @@ const CategoryButton: React.FC<historyProps> = (props) => {
             {categoryList.map((category) => (
                 <div key={category} className="categoryButton">
                     <button key={category} onClick={() => {
-                        categorySearch(category)
                         addTag(category)
-                        props.history.push('/result')
+                        props.history.push('/search?tag=' + category)
                     }}>
-                        <img src={ "./img/" + category + ".png" } alt={ category + "の写真" }></img>
+                        <img src={"./img/" + category + ".png"} alt={category + "の写真"}></img>
                         <div className="categoryName">
-                            { category }
+                            {category}
                         </div>
                     </button>
                 </div>
--- src/components/systemList.tsx
+++ src/components/systemList.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../store'
 import { System } from '../reducers/systemsReducer'
-import { updateDetailCreator } from '../actions/action'
+import { updateDetailCreator, fetchSystemByCategory } from '../actions/action'
 import Indicator from './indicator'
 import { withRouter, RouteComponentProps } from 'react-router'
+import { parse } from 'query-string'
 import "../scss/systemList.scss"
 
 type historyProps = RouteComponentProps
 
 const SystemList: React.FC<historyProps> = (props) => {
+    // TODO: 無理くりすぎ →　as とか使うのがいい？
+    const tag = String(parse(props.location.search).tag)
+    console.log(tag)
     const systems = useSelector((state: AppState) => state.systemsState.systems)
     const loading = useSelector((state: AppState) => state.systemsState.loading)
     const dispatch = useDispatch()
     const updateDetail = (selectData: System) => dispatch(updateDetailCreator(selectData))
+    
+    /* TODO: タグなしの処理 */
+    //データのfetch
+    useEffect(() => {
+        const categorySearch = (category: string) => dispatch(fetchSystemByCategory(category))
+        categorySearch(tag)
+    }, [dispatch,tag])
+
     return (
         <div className="systemList">
             {console.log('loading:', loading)}
@@ -25,7 +37,7 @@ const SystemList: React.FC<historyProps> = (props) => {
                     <ul>
                         {systems.map((system: System) => (
                             <li key={system.Name} onClick={() => {
-                                updateDetail(system)
+                                updateDetail(system)    //リロードなしでページを遷移させるのに必要
                                 props.history.push('/detail/' + system.documentID)
                             }
                             }>
