a042e3c327b628f3866119f91d06418d4a29d55f
sasa-kou <sasakou11428@gmail.com>
2019-08-13 18:44:36 +0900

    fix as string

--- src/components/systemList.tsx
+++ src/components/systemList.tsx
@@ -11,41 +11,39 @@ import "../scss/systemList.scss"
 type historyProps = RouteComponentProps
 
 const SystemList: React.FC<historyProps> = (props) => {
-    // TODO: 無理くりすぎ →　as とか使うのがいい？
-    const tag = String(parse(props.location.search).tag)
+    const tag = parse(props.location.search).tag as string
     console.log(tag)
     const systems = useSelector((state: AppState) => state.systemsState.systems)
     const loading = useSelector((state: AppState) => state.systemsState.loading)
     const dispatch = useDispatch()
     const updateDetail = (selectData: System) => dispatch(updateDetailCreator(selectData))
-    
-    /* TODO: タグなしの処理 */
+
     //データのfetch
     useEffect(() => {
         const categorySearch = (category: string) => dispatch(fetchSystemByCategory(category))
-        categorySearch(tag)
-    }, [dispatch,tag])
+        /* TODO: タグなしの処理 */
+        if (tag !== undefined) {
+            categorySearch(tag)
+        }
+    }, [dispatch, tag])
 
     return (
         <div className="systemList">
             {console.log('loading:', loading)}
             {console.log('systems:', systems)}
             {loading ? <Indicator /> :
-                systems.length === 0 ?
-                    <p>検索結果がありません</p>
-                    :
-                    <ul>
-                        {systems.map((system: System) => (
-                            <li key={system.Name} onClick={() => {
-                                updateDetail(system)    //リロードなしでページを遷移させるのに必要
-                                props.history.push('/detail/' + system.documentID)
-                            }
-                            }>
-                                <h4>{system.Name}</h4>
-                                <p>{system.Location}</p>
-                            </li>
-                        ))}
-                    </ul>
+                <ul>
+                    {systems.map((system: System) => (
+                        <li key={system.Name} onClick={() => {
+                            updateDetail(system)    //リロードなしでページを遷移させるのに必要
+                            props.history.push('/detail/' + system.documentID)
+                        }
+                        }>
+                            <h4>{system.Name}</h4>
+                            <p>{system.Location}</p>
+                        </li>
+                    ))}
+                </ul>
             }
         </div>
     )
