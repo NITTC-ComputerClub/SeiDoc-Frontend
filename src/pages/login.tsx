f02fe5986d139d127c7163e57f6dbdef3c4d4293
Hitoki Minami <mkmokymh@gmail.com>
2019-08-15 08:24:51 +0900

    add: login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
 import SignIn from '../components/Login/signIn'
+import "../scss/login.scss"
+import Footer from  '../components/footer'
 
 type historyProps = RouteComponentProps
 
 const Login: React.FC<historyProps> = (props) => {
     return (
-        <div>
-            <p>ログイン</p>
-            <SignIn />
+        <div className="login">
+            <div className="loginForm">
+                <div className="title">
+                    <img src="/img/logo.png" alt="SeiDocのロゴ"></img>
+                    <h2>ログイン</h2>
+                </div>
+                <SignIn />
+            </div>
+            <Footer />
         </div>
     )
 }
