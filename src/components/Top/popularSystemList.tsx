4c0d4182e4cc365b46a6dd8d11bc66d3a36c850f
Takamichi Omori <merrytakamerry@gmail.com>
2019-08-15 21:15:53 +0900

    ちょっときれいにした

--- src/components/Top/popularSystemList.tsx
+++ src/components/Top/popularSystemList.tsx
@@ -1,35 +1,6 @@
 import React, { useState } from 'react'
 import PopularSystemCard from './popularSystemCard'
-import { fireStore, popularPageIndex, rankingType, detailPageLogIndex, logType } from '../../firebase/firebase';
-
-
-type rankings = {
-    ranking: rankingType[]
-}
-const compareASC = (a: rankingType,b: rankingType) => {
-    if(a.count < b.count){
-        return -1
-    }else{
-        return 1
-    }
-}
-
-  const getAWeekAgo = () => {
-    const dt = new Date();
-    const y = dt.getFullYear();
-    const m = ("00" + (dt.getMonth()+1)).slice(-2);
-    const d = ("00" + dt.getDate()).slice(-2);
-    const result = new Date(y,parseInt(m),parseInt(d)-7)    
-    return result;
-}
-const getToday = () => {
-    const dt = new Date();
-    const y = dt.getFullYear();
-    const m = ("00" + (dt.getMonth()+1)).slice(-2);
-    const d = ("00" + dt.getDate()).slice(-2);
-    const result = y + "-" + m + "-" + d;
-    return result;
-}
+import { fireStore, rankingType, detailPageLogIndex, logType } from '../../firebase/firebase';
 
 const PopularSystemList: React.FC = () => {
     const [rankingData, setRankingData] = useState<rankingType[]>([{count:-1, systemName: '-1', systemLocation: 'none', documentID: 'XXX'}])
@@ -44,17 +15,14 @@ const PopularSystemList: React.FC = () => {
 
     if(rankingData[0].count === -1){ //一度だけfetch
         const today = Date.now()
-        const aWeekAgo = getAWeekAgo()
         const ranking: rankingType[] = [];
-        console.log('today',today,'a week ago', aWeekAgo)
         fireStore
-        .collection(detailPageLogIndex).where("createdAt", "<",today)
+        .collection(detailPageLogIndex).where("createdAt", "<", today).where("createdAt", "<", today-604800)
         .get()
         .then(snapshot => {
-          console.log('fetched', snapshot.docs)
           snapshot.forEach(doc => {
               const data = doc.data() as logType
-              console.log(data)
+              //console.log(data)
               const target = ranking.find(logData => {return logData.documentID === data.documentID})
               if(target === undefined){
                   const r: rankingType = {
