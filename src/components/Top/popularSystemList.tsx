4088a0d96a3a32ed4510a85ed2171de43bf4f6d8
sasa-kou <sasakou11428@gmail.com>
2019-08-18 23:04:00 +0900

    こゆこと？

--- src/components/Top/popularSystemList.tsx
+++ src/components/Top/popularSystemList.tsx
@@ -5,54 +5,53 @@ import Indicator from '../indicator'
 import "../../scss/popularSystemList.scss"
 
 const PopularSystemList: React.FC = () => {
-    const [rankingData, setRankingData] = useState<rankingType[]>([{count:-1, systemName: '-1', systemLocation: 'none', documentID: 'XXX'}])
-    
+    const [rankingData, setRankingData] = useState<rankingType[]>([{ count: -1, system: { Name: '', Department: '', Location: '', Site: '', Detail: '', Target: '', Method: [], Category: [], CreatedAt: 0, UpdatedAt: 0, isDeleted: false, ExpireAt: 0, documentID: '-1', }, documentID: 'XXX' }])
+
     const isLoaded = () => {
-        if(rankingData[0].count !== -1){
+        if (rankingData[0].count !== -1) {
             return true
-        }else{
+        } else {
             return false
         }
     }
 
-    if(rankingData[0].count === -1){ //一度だけfetch
+    if (rankingData[0].count === -1) { //一度だけfetch
         const today = Date.now()
         const ranking: rankingType[] = [];
         fireStore
-        .collection(detailPageLogIndex).where("createdAt", "<", today).where("createdAt", "<", today-604800)
-        .get()
-        .then(snapshot => {
-          snapshot.forEach(doc => {
-              const data = doc.data() as logType
-              //console.log(data)
-              const target = ranking.find(logData => {return logData.documentID === data.documentID})
-              if(target === undefined){
-                  const r: rankingType = {
-                      documentID : data.documentID,
-                      systemName: data.system.Name,
-                      systemLocation: data.system.Location,
-                      count: 1
-                  }
-                  ranking.push(r)
-              }else{
-                  target.count++
-              }
-          });
-        })
-        .then(() => {
-            console.log(ranking)
-            setRankingData(ranking.slice(0,3))
-        }).catch(err => {console.error(err);});
+            .collection(detailPageLogIndex).where("createdAt", "<", today).where("createdAt", "<", today - 604800)
+            .get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    const data = doc.data() as logType
+                    //console.log(data)
+                    const target = ranking.find(logData => { return logData.documentID === data.documentID })
+                    if (target === undefined) {
+                        const r: rankingType = {
+                            documentID: data.documentID,
+                            system: data.system,
+                            count: 1
+                        }
+                        ranking.push(r)
+                    } else {
+                        target.count++
+                    }
+                });
+            })
+            .then(() => {
+                console.log(ranking)
+                setRankingData(ranking.slice(0, 3))
+            }).catch(err => { console.error(err); });
     }
-    return(
+    return (
         isLoaded() ?
-        <div className="popularSystemList">
-            <ul>
-                {rankingData.map(data => <PopularSystemCard key={data.systemName} systemName={data.systemName} systemLocation={data.systemLocation} documentId={data.documentID}/>)}
-            </ul>
-        </div>
-        :
-        <Indicator />
+            <div className="popularSystemList">
+                <ul>
+                    {rankingData.map(data => <PopularSystemCard key={data.system.Name} system={data.system} />)}
+                </ul>
+            </div>
+            :
+            <Indicator />
     )
 }
 
