8b83373609efb1cbde50d9cf979de66cff81507a
sasa-kou <sasakou11428@gmail.com>
2019-08-30 22:41:18 +0900

    難しすぎ

--- src/pages/machineLearning.tsx
+++ src/pages/machineLearning.tsx
@@ -5,6 +5,7 @@ import { machineLearningType } from '../types/type'
 const MachineLearning: React.FC = () => {
     const [cvsWidth, setCvsWidth] = useState<number>(0)
     const [cvsHeight, setCvsHeight] = useState<number>(0)
+    const ageText: Array<string> = ['']
 
     const machineLearning = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files as FileList
@@ -46,19 +47,43 @@ const MachineLearning: React.FC = () => {
             img.onload = () => {
                 const shrinkW = 600 / img.width
                 const shrinkH = 400 / img.height
-                context.drawImage(img, 0, 0, 600, 400)
+                context.drawImage(img, 0, 0, 600, 400)  //写真描画
+
                 data.forEach((value) => {
                     let heigh = value.face_location.height * shrinkH
                     let left = value.face_location.left * shrinkW
                     let top = value.face_location.top * shrinkH
                     let width = value.face_location.width * shrinkW
 
+                    /* 顔に四角を生成 */
                     context.strokeRect(left, top, width, heigh)
+
+
+                    /* テキストボックスを生成 */
+                    const textarea = document.createElement('input')
+                    textarea.className = 'info' // これでCSS当てられる？
+                    let age = (value.age.max + value.age.min) / 2
+
+                    textarea.value = age.toString()
+                    textarea.name = age.toString()
+                    textarea.onchange = (e) => handleInputChange(e)
+                    textarea.style.position = 'absolute'
+                    textarea.style.top = top - 30 + 'px'
+                    textarea.style.left = left + 'px'
+
+                    document.body.appendChild(textarea)
                 })
             }
         }
         reader.readAsDataURL(file)
     }
+
+    const handleInputChange = (e: Event) => {
+        const value = e.target as EventTarget
+        console.log(value)
+        console.log(e)
+    }
+
     return (
         <div>
             <canvas id='cvs' width='600' height='400'></canvas>
