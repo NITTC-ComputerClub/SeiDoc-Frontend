756f1044f3685316c8541403ba1c38b4da684874
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-02 22:39:22 +0900

    いったんふつうのHeader

--- src/pages/input.tsx
+++ src/pages/input.tsx
@@ -3,6 +3,8 @@ import { fireStore, systemIndex } from '../firebase/firebase'
 import { AppState } from '../store';
 import { useSelector } from 'react-redux';
 import { sendData } from '../types/type';
+import Header from './header';
+import Footer from '../components/footer';
 
 
 
@@ -52,39 +54,43 @@ const Input: React.FC = () => {
 
     return (
         <div>
-            <h3>制度登録</h3>
-            <input type='text' onChange={e => { name = e.target.value }} placeholder="制度名を入力"/>
-            <h5>対象地区</h5>
-            <input type='text' defaultValue={user.city} />
-            <h5>カテゴリ</h5>
-            <select onChange={() => {}}>
-                <option defaultChecked>カテゴリを選択</option>
-                <option value="子育て">子育て</option>
-                <option value="介護">介護</option>
-                <option value="建築">建築</option>
-                <option value="病気">病気</option>
-                <option value="融資">融資</option>
-                <option value="地域">地域</option>
-                <option value="高齢者">高齢者</option>
-                <option value="その他">その他</option>
-            </select>
-            <h5>援助対象者</h5>
-            <input type='text' onChange={e => { target = e.target.value }} />
-            <h5>援助方法</h5>
-            <select onChange={e => { sysmethod = [e.target.value] }}>
-                <option value='金銭補助' >金銭補助</option>
-                <option value='権利譲渡' >権利譲渡</option>
-                <option value='物品支給' >物品支給</option>
-                <option value='その他' >その他</option>
-            </select>
-            <h5>担当部署</h5>
-            <input type='text' onChange={e => { department = e.target.value }} />
-            <h5>詳細</h5>
-            <textarea onChange={e => { detail = e.target.value }} />
-            <h5>公式のページ</h5>
-            <input type='text' onChange={e => { site = e.target.value }} />
-            <br />
-            <button onClick={post}>登録</button>
+            <Header />
+            <div>
+                <h3>制度登録</h3>
+                <input type='text' onChange={e => { name = e.target.value }} placeholder="制度名を入力"/>
+                <h5>対象地区</h5>
+                <input type='text' defaultValue={user.city} />
+                <h5>カテゴリ</h5>
+                <select onChange={() => {}}>
+                    <option defaultChecked>カテゴリを選択</option>
+                    <option value="子育て">子育て</option>
+                    <option value="介護">介護</option>
+                    <option value="建築">建築</option>
+                    <option value="病気">病気</option>
+                    <option value="融資">融資</option>
+                    <option value="地域">地域</option>
+                    <option value="高齢者">高齢者</option>
+                    <option value="その他">その他</option>
+                </select>
+                <h5>援助対象者</h5>
+                <input type='text' onChange={e => { target = e.target.value }} />
+                <h5>援助方法</h5>
+                <select onChange={e => { sysmethod = [e.target.value] }}>
+                    <option value='金銭補助' >金銭補助</option>
+                    <option value='権利譲渡' >権利譲渡</option>
+                    <option value='物品支給' >物品支給</option>
+                    <option value='その他' >その他</option>
+                </select>
+                <h5>担当部署</h5>
+                <input type='text' onChange={e => { department = e.target.value }} />
+                <h5>詳細</h5>
+                <textarea onChange={e => { detail = e.target.value }} />
+                <h5>公式のページ</h5>
+                <input type='text' onChange={e => { site = e.target.value }} />
+                <br />
+                <button onClick={post}>登録</button>
+            </div>
+            <Footer />
         </div>
     )
 }
