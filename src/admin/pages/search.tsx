fbdff2b7292a92f758eff9aacefdc8e20b60e978
watamoko <kei07161@yahoo.co.jp>
2019-09-09 23:08:38 +0900

    search複製

---
+++ src/admin/pages/search.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SearchBar from '../../components/searchBar'
+import Tag from '../../components/tag'
+import AdminSystemList from '../components/adminSystemList'
+import Footer from '../../components/footer'
+import Header from '../components/header'
+
+const Search: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <div className="result">
+                <div className="container">
+                    <SearchBar pushTo="/admin/search" />
+                </div>
+                <Tag pushTo="/admin/category" />
+                <h3>検索結果</h3>
+                <AdminSystemList />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
