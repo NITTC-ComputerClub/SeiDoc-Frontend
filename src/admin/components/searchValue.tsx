8fc33a43ce19e0354de6235674cd2b5dac692a46
watamoko <kei07161@yahoo.co.jp>
2019-09-11 02:30:55 +0900

    タグ検索で検索結果の文を非表示に

--- src/admin/components/searchValue.tsx
+++ src/admin/components/searchValue.tsx
@@ -7,11 +7,11 @@ type historyProps = RouteComponentProps
 const SearchValue: React.FC<historyProps> = (props) => {
     const tag = parse(props.location.search).tag as string
     const inputValue = parse(props.location.search).value as string
-    if (tag !== undefined && inputValue !== undefined) {
+    if (tag !== undefined && inputValue !== undefined) { //アルゴリアサーチ
         return (<h2>「{inputValue}」の検索結果</h2>)
     }
-    else if (tag !== undefined && inputValue === undefined) {
-        return (<h2>「{tag}」の検索結果</h2>)
+    else if (tag !== undefined && inputValue === undefined) { //タグ検索
+        return (<div></div>)
     }
     else {
         return (<div></div>)
