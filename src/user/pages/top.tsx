c3d189e7b214b26db7ff6b6ed510ebe8f11a139e
sasa-kou <sasakou11428@gmail.com>
2019-09-11 16:05:38 +0900

    rm 不要なimport

--- src/user/pages/top.tsx
+++ src/user/pages/top.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { AppState } from '../../store'
 import SearchButton from "../components/Top/searchButton";
 import PopularSystemList from "../components/Top/popularSystemList";
