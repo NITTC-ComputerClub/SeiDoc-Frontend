6ba943e555ceaa0a8b1e9e2b48a2bb83b171dc49
sasa-kou <sasakou11428@gmail.com>
2019-09-11 15:00:21 +0900

    タグの削除機能を廃止

--- src/user/components/tag.tsx
+++ src/user/components/tag.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { AppState } from '../../store'
-import { deleteTagCreator, deleteSystemsCreator } from '../../actions/action'
 import { withRouter, RouteComponentProps } from 'react-router'
 import '../../scss/tag.scss'
 
@@ -11,18 +10,11 @@ interface historyProps extends RouteComponentProps {
 
 const Tag: React.FC<historyProps> = (props) => {
     const tag = useSelector((state: AppState) => state.tagState.tag)
-    const dispatch = useDispatch()
-    const deleteTag = () => dispatch(deleteTagCreator())
-    const deleteSystems = () => dispatch(deleteSystemsCreator())
     return (
         <div className="tag">
             {tag === '' ?
                 <div></div> :
-                <button key={tag} onClick={() => {
-                    deleteTag()
-                    deleteSystems()
-                    props.history.push(props.pushTo)
-                }}>{tag}<span>×</span></button>}
+                <p>{tag}</p>}
         </div>
     )
 }
