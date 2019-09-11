3d7270cba8b69416a65f844ee17e45f364b27b00
watamoko <kei07161@yahoo.co.jp>
2019-09-11 11:04:51 +0900

    クエリとタグ検索でタグの表示を分けた

--- src/user/components/tag.tsx
+++ src/user/components/tag.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../../store'
 import { deleteTagCreator, deleteSystemsCreator } from '../../actions/action'
 import { withRouter, RouteComponentProps } from 'react-router'
+import { parse } from 'query-string'
 import '../../scss/tag.scss'
 
 interface historyProps extends RouteComponentProps {
@@ -11,20 +12,23 @@ interface historyProps extends RouteComponentProps {
 
 const Tag: React.FC<historyProps> = (props) => {
     const tag = useSelector((state: AppState) => state.tagState.tag)
+    const inputValue = parse(props.location.search).value as string
     const dispatch = useDispatch()
     const deleteTag = () => dispatch(deleteTagCreator())
     const deleteSystems = () => dispatch(deleteSystemsCreator())
-    return (
-        <div className="tag">
-            {tag === '' ?
-                <div></div> :
-                <button key={tag} onClick={() => {
-                    deleteTag()
-                    deleteSystems()
-                    props.history.push(props.pushTo)
-                }}>{tag}<span>×</span></button>}
-        </div>
-    )
+    if (inputValue === undefined)
+        return (
+            <div className="tag">
+                {tag === '' ?
+                    <div></div> :
+                    <button key={tag} onClick={() => {
+                        deleteTag()
+                        deleteSystems()
+                        props.history.push(props.pushTo)
+                    }}>{tag}<span>×</span></button>}
+            </div>
+        )
+    else return <div></div>
 }
 
 export default withRouter<historyProps, React.FC<historyProps>>(Tag)
