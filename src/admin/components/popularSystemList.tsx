caba1d3e73b1efefea0010a1bc443497ab32cbda
watamoko <kei07161@yahoo.co.jp>
2019-09-13 00:38:09 +0900

    制度のカードを押したらdetailに飛べるけどなんかエラーはいた

--- src/admin/components/popularSystemList.tsx
+++ src/admin/components/popularSystemList.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 import { fireStore, systemIndex } from '../../firebase/firebase'
-import { System } from '../../types/type'
+import { useDispatch } from 'react-redux'
+import { updateDetailCreator } from '../../actions/action'
+import { withRouter, RouteComponentProps } from 'react-router'
 import Indicator from '../../user/components/indicator'
+import { System } from '../../types/type'
 
-const AdminPopularSystemList: React.FC = () => {
+type historyProps = RouteComponentProps
+
+const AdminPopularSystemList: React.FC<historyProps> = (props) => {
     const [popularData, setPopularData] = useState<System[]>([])
     const [isLoading ,setIsLoading] = useState<boolean>(false)
-
-    const popularDataArray :System[] = [];
+    const popularDataArray :System[] = []
+    const dispatch = useDispatch()
+    const updateDetail = (selectData: System) => dispatch(updateDetailCreator(selectData))
 
     if(popularData.length === 0){
     fireStore.collection(systemIndex).orderBy("monthlyView", "desc").limit(4).get()
@@ -19,19 +25,21 @@ const AdminPopularSystemList: React.FC = () => {
             }
         ).then(
             () => {
-                setPopularData(popularDataArray);
+                setPopularData(popularDataArray)
                 setIsLoading(true)
             }
         )
     }
     console.log(popularData)
 
-
     return isLoading ? (
         <div>
             <ul>
                 {popularData.map((system: System) => (
-                    <li key={system.Name}>
+                    <li key={system.Name} onClick={() => {
+                        updateDetail(system)    //リロードなしでページを遷移させるのに必要
+                        props.history.push('/admin/detail/' + system.documentID)
+                    }}>
                         <h4>{system.Name}</h4>
                         <h6>閲覧数 {system.monthlyView}/月</h6>
                         <p>{system.ageGroup[0].age}に人気</p>
@@ -44,4 +52,4 @@ const AdminPopularSystemList: React.FC = () => {
     )
 }
 
-export default AdminPopularSystemList
+export default withRouter<historyProps, React.FC<historyProps>>(AdminPopularSystemList)
