4664cb6f27043d33049c06516aa53f8edd281790
watamoko <kei07161@yahoo.co.jp>
2019-09-14 16:47:42 +0900

    ファイル作った

---
+++ src/admin/components/searchWords.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { fireStore, searchLogIndex } from '../../firebase/firebase'
+import Indicator from '../../user/components/indicator'
+import { searchLogType } from '../../types/type'
+import { firestore } from 'firebase'
+
+const SearchWords: React.FC = () => {
+    const [searchData, setSearchData] = useState<searchLogType[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const searchDataArray: searchLogType[] = []
+
+    if (searchData.length === 0) {
+        fireStore.collection(searchLogIndex).orderBy("createdAt", "desc").limit(10).get()
+        .then(
+            (snapshot) => {
+                snapshot.forEach((doc) => {
+                    searchDataArray.push(doc.data() as searchLogType)
+                })
+            }
+        ).then(
+            () => {
+                setSearchData(searchDataArray)
+            }
+        )
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SearchWords
