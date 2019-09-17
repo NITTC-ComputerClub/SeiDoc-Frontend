ba74615775134281bb522ad086075a5b580ad8f8
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-17 23:35:48 +0900

    つくりはじめた

--- src/admin/pages/csvDownload.tsx
+++ src/admin/pages/csvDownload.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../components/header'
+import Footer from '../../user/components/footer-pc'
+import styled from 'styled-components';
+
+
+const Select = styled.select` 
+    width: 100%;
+    height: 36px;
+    margin-bottom: 16px;
+    padding: 8px;
+    border-radius: 2px;
+    border: none;
+    vertical-align: middle;
+`
 
 const CSVDownload: React.FC = () => {
+    const [isName, setIsName] = useState<boolean>(false)
     return (
         <div>
-            CSV ダウンロード画面 :smile:
+            <Header />
+            <div>
+                <h5>データ内容</h5>
+                <Select>
+                    <option value="すべて">全カテゴリ</option>
+                    <option value="子育て">子育て</option>
+                    <option value="介護">介護</option>
+                    <option value="建築">建築</option>
+                    <option value="病気">病気</option>
+                    <option value="融資">融資</option>
+                    <option value="地域">地域</option>
+                    <option value="高齢者">高齢者</option>
+                    <option value="その他">その他</option>
+                </Select>
+            </div>
+            <div>
+                <h2>項目</h2>
+                <label key="制度" >
+                <h5>制度名</h5>
+                <input
+                    type="checkbox"
+                    checked={isName}
+                    onChange={() => setIsName(!isName)}
+                />
+                </label>
+        </div>  
+        <Footer />
         </div>
     )
 }
