45e4c5b26f715e37bcd5b1249204c47a2e8df6ae
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-17 20:05:56 +0900

    initial commit

---
+++ src/admin/pages/csvDownload.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CSVDownload: React.FC = () => {
+    return (
+        <div>
+            CSV ダウンロード画面 :smile:
+        </div>
+    )
+}
+
+export default CSVDownload
