7044ee4e4170d01e3705efcbf17de2aca27d99b9
sasa-kou <sasakou11428@gmail.com>
2019-09-18 18:07:40 +0900

    あともう少し

--- src/user/components/Login/fixProfile.tsx
+++ src/user/components/Login/fixProfile.tsx
@@ -1,13 +1,15 @@
-import React, { useState, SetStateAction } from 'react'
+import React, { useState } from 'react'
+import drawProfile from './drawProfile'
 import { profileDataType } from '../../../types/type'
 
 type propsType = {
     profileData: Array<profileDataType>,
-    setProfileData: React.Dispatch<SetStateAction<Array<profileDataType>>>
+    setProfileData: React.Dispatch<React.SetStateAction<Array<profileDataType>>>
 }
 
 const FixProfile: React.FC<propsType> = (props) => {
     const canvas = document.getElementById('cvs') as HTMLCanvasElement
+    const [sequence, setSequence] = useState<number>(0)
 
     const onClick = (e: MouseEvent) => {
         const inputAge = document.getElementById('age') as HTMLInputElement
@@ -16,28 +18,41 @@ const FixProfile: React.FC<propsType> = (props) => {
         const x = e.clientX - rect.left
         const y = e.clientY - rect.top
 
-        props.profileData.forEach(element => {
+        props.profileData.forEach((element, index) => {
             if (element.boundingBox.left <= x && x <= (element.boundingBox.left + element.boundingBox.width)
                 && element.boundingBox.top <= y && y <= (element.boundingBox.top + element.boundingBox.height)) {
                 for (let i = 0; i < selectRelationship.length; i++) {
                     if (selectRelationship.options[i].value === element.relationship) {
                         selectRelationship.selectedIndex = i
                         inputAge.value = element.age.toString()
+                        setSequence(index)
                         break
                     }
                 }
-                
                 //TODO 性別どうするよ
             }
         })
+    }
+
+    const fetchData = () => {
+        const inputAge = document.getElementById('age') as HTMLInputElement
+        const selectRelationship = document.getElementById('relationship') as HTMLSelectElement
+        const value: profileDataType = props.profileData[sequence]
+        value.age = Number(inputAge.value)
+        value.relationship = selectRelationship.value
+        console.log(value)
+        props.profileData.splice(sequence, 1, value)
+        console.log(props.profileData)
+        props.setProfileData(props.profileData)
     }
 
     canvas.addEventListener('click', onClick, false)
 
     return (
         <div>
+            {drawProfile(props.profileData)}
             <p>関係</p>
-            <select id='relationship' onChange={(e) => console.log('')}>
+            <select id='relationship' className='fix'>
                 <option value='本人'>本人</option>
                 <option value='夫'>夫</option>
                 <option value='妻'>妻</option>
@@ -47,7 +62,8 @@ const FixProfile: React.FC<propsType> = (props) => {
                 <option value='祖母'>祖母</option>
             </select>
             <p>年齢</p>
-            <input id='age' type="text" onChange={(e) => console.log('')}></input>
+            <input id='age' type="text" className='fix'></input>
+            <button onClick={fetchData}>保存</button>
         </div>
     )
 }
