3ddbcaa73a7b7ea128331d090d3fa7ec40e8299d
sasa-kou <sasakou11428@gmail.com>
2019-09-18 13:27:22 +0900

    残すは値の変更のみ！

--- src/user/components/Login/fixProfile.tsx
+++ src/user/components/Login/fixProfile.tsx
@@ -8,29 +8,25 @@ type propsType = {
 
 const FixProfile: React.FC<propsType> = (props) => {
     const canvas = document.getElementById('cvs') as HTMLCanvasElement
-    const [relationship, setRelationship] = useState<string>('')
-    const [age, setAge] = useState<number>(0)
 
     const onClick = (e: MouseEvent) => {
+        const inputAge = document.getElementById('age') as HTMLInputElement
         const selectRelationship = document.getElementById('relationship') as HTMLSelectElement
         const rect = canvas.getBoundingClientRect()
         const x = e.clientX - rect.left
         const y = e.clientY - rect.top
-        let sequence = 0
-        console.log(x, y)
+
         props.profileData.forEach(element => {
             if (element.boundingBox.left <= x && x <= (element.boundingBox.left + element.boundingBox.width)
                 && element.boundingBox.top <= y && y <= (element.boundingBox.top + element.boundingBox.height)) {
-                setRelationship(element.relationship)
-                setAge(element.age)
                 for (let i = 0; i < selectRelationship.length; i++) {
                     if (selectRelationship.options[i].value === element.relationship) {
                         selectRelationship.selectedIndex = i
+                        inputAge.value = element.age.toString()
                         break
                     }
                 }
-                console.log('関係', relationship)
-                console.log('年齢', age)
+                
                 //TODO 性別どうするよ
             }
         })
@@ -41,7 +37,7 @@ const FixProfile: React.FC<propsType> = (props) => {
     return (
         <div>
             <p>関係</p>
-            <select id='relationship' onChange={(e) => setRelationship(e.target.value)}>
+            <select id='relationship' onChange={(e) => console.log('')}>
                 <option value='本人'>本人</option>
                 <option value='夫'>夫</option>
                 <option value='妻'>妻</option>
@@ -51,7 +47,7 @@ const FixProfile: React.FC<propsType> = (props) => {
                 <option value='祖母'>祖母</option>
             </select>
             <p>年齢</p>
-            <input type="text" value={age} onChange={(e) => setAge(Number(e.target.value))}></input>
+            <input id='age' type="text" onChange={(e) => console.log('')}></input>
         </div>
     )
 }
