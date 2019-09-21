939e56cc84b8146fdc685ae95b485d3046152a51
sasa-kou <sasakou11428@gmail.com>
2019-09-22 00:00:22 +0900

    直した

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAG2zF0FZmWoOxiNoMbSAwKXXmgKw6X8t0",
+    authDomain: "sei-doc.firebaseapp.com",
+    databaseURL: "https://sei-doc.firebaseio.com",
+    projectId: "sei-doc",
+    storageBucket: "sei-doc.appspot.com",
+    messagingSenderId: "485197181540",
+    appId: "1:485197181540:web:099d1cb8d9652170"
+  };
+  
+  export default firebaseConfig;
