9e5b5e8022da281fc415191d3cfb1908d45431eb
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-25 17:33:20 +0900

    remove unused inport

--- src/admin/components/adminSystemList.tsx
+++ src/admin/components/adminSystemList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../../store'
 import { updateDetailCreator, fetchSystemByCategory, deleteSystemsCreator, addTagCreator, fetchSystemByAlgoliaSearch } from '../../actions/action'
