551a64bfb7c39ad45f4b9eb234f82dc482f52b5f
asasigure <31458031+asasigure1209@users.noreply.github.com>
2019-09-25 20:26:21 +0900

    /searchのタグにCSSを当てました (#245)

--- src/user/components/tag.tsx
+++ src/user/components/tag.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { AppState } from '../../store'
-import '../../scss/tag.scss'
+import styled from 'styled-components'
+import setting from '../../designSystem/setting'
 
+const StyledTag = styled.p`
+    display: inline-block;
+    margin: 0 0 0 16px;
+    color: ${setting.White};
+    background-color: ${setting.ThemeBlue};
+    padding: 4px 8px;
+    border-radius: 4px;
+    font-size: ${setting.P2};
+`
 
 const Tag: React.FC = () => {
     const tag = useSelector((state: AppState) => state.tagState.tag)
@@ -10,7 +20,7 @@ const Tag: React.FC = () => {
         <div className="tag">
             {tag === '' ?
                 <div></div> :
-                <p>{tag}</p>}
+                <StyledTag>{tag}</StyledTag>}
         </div>
     )
 }
