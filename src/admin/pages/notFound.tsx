f6d3f5e55eeec57a6e8c82bfaf4ffa6ebc7a4b00
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-27 12:49:33 +0900

    isAdminで出し分け

--- src/admin/pages/notFound.tsx
+++ src/admin/pages/notFound.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Wrapper } from '../../designSystem/Page';
-import Header from '../components/header';
-import Footer from '../../user/components/footer-pc';
+import AdminHeader from '../components/header';
+import AdminFooter from '../../user/components/footer-pc';
+import UserHeader from '../../user/components/header'
+import UserFooter from '../../user/components/footer'
+import { useSelector } from 'react-redux';
+import { AppState } from '../../store';
 
 
 const NotFound: React.FC = () => {
-    return(
-       <Wrapper>
-           <Header />
-            <div>
-                <p>404</p>
-                <p>Not Found</p>
-                <p>お探しのページは見つかりませんでした。</p>
-            </div>
-            <Footer />
+    const user = useSelector((state: AppState) => state.userState)
+
+    if(user.isAdmin){
+        return (
+            <Wrapper>
+                <AdminHeader />
+                    <div>
+                        <p>404</p>
+                        <p>Not Found</p>
+                        <p>お探しのページは見つかりませんでした。</p>
+                    </div>
+                <AdminFooter />
            </Wrapper> 
-    )
+        )
+    } else {
+        return (
+            <Wrapper>
+                <UserHeader />
+                    <div>
+                        <p>404</p>
+                        <p>Not Found</p>
+                        <p>お探しのページは見つかりませんでした。</p>
+                    </div>
+                <UserFooter />
+            </Wrapper>
+        )
+    }
 }
 
 export default NotFound
