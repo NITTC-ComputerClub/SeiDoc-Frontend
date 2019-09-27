96be2eea5299e5ccd307def1786beccb063e8c7f
sasa-kou <sasakou11428@gmail.com>
2019-09-27 12:53:40 +0900

    地域から調べるボタンの遷移先を作成

---
+++ src/user/components/regionButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegionButton: React.FC = () => {
+    return (
+        <div>
+            <button>愛知県名古屋市</button>
+            <button>愛知県小牧市</button>
+        </div>
+    )
+}
+export default RegionButton
