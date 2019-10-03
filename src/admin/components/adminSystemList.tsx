09eabffff030ad5f82fdec41af30c10e7b0e75f2
asasigure <31458031+asasigure1209@users.noreply.github.com>
2019-10-03 17:35:21 +0900

    admin/searchの制度カードに「人気の世代」を追加 (#259)

--- src/admin/components/adminSystemList.tsx
+++ src/admin/components/adminSystemList.tsx
@@ -45,11 +45,15 @@ const AdminSystemCard = styled.li`
   p {
     font-size: ${setting.P1};
   }
-
   .view {
-    margin-top: 8px;
+      margin-top: 8px;
+  }
+
+  .blue {
+      color: ${setting.ThemeBlue};
   }
 `;
+
 
 const AdminSystemList: React.FC<historyProps> = props => {
   const tag = parse(props.location.search).tag as string;
@@ -77,32 +81,27 @@ const AdminSystemList: React.FC<historyProps> = props => {
     }
   }, [dispatch, tag, inputValue, region]);
 
-  return (
-    <div>
-      {console.log("systems:", systems)}
-      {loading ? (
-        <Indicator />
-      ) : (
-        <Grid>
-          {systems.map((system: System) => (
-            <AdminSystemCard
-              key={system.Name}
-              onClick={() => {
-                updateDetail(system); //リロードなしでページを遷移させるのに必要
-                props.history.push("/admin/detail/" + system.documentID);
-              }}
-            >
-              {console.log(system)}
-              <h2>{system.Name}</h2>
-              <p>{system.Department}</p>
-              <p className="view">閲覧数　{system.monthlyView}回/月</p>
-            </AdminSystemCard>
-          ))}
-        </Grid>
-      )}
-    </div>
-  );
-};
-export default withRouter<historyProps, React.FC<historyProps>>(
-  AdminSystemList
-);
+    return (
+        <div>
+            {console.log('systems:', systems)}
+            {loading ? <Indicator /> :
+                <Grid>
+                    {systems.map((system: System) => (
+                        <AdminSystemCard key={system.Name} onClick={() => {
+                            updateDetail(system);    //リロードなしでページを遷移させるのに必要
+                            props.history.push('/admin/detail/' + system.documentID);
+                        }
+                        }>
+                            {console.log(system)}
+                            <h2>{system.Name}</h2>
+                            <p>{system.Department}</p>
+                            <p className="view">閲覧数　{system.monthlyView}回/月</p>
+                            {system.ageGroup[0] ? <p className="blue">{system.ageGroup[0].age}代に人気</p> : ""}
+                        </AdminSystemCard>
+                    ))}
+                </Grid>
+            }
+        </div>
+    )
+}
+export default withRouter<historyProps, React.FC<historyProps>>(AdminSystemList)
