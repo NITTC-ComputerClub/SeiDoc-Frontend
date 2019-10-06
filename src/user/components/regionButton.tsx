012fdb656606a371d78c41b56c734f5093b5574b
sasa-kou <sasakou11428@gmail.com>
2019-10-06 22:21:57 +0900

    コンポーネントの統一か

--- src/user/components/regionButton.tsx
+++ src/user/components/regionButton.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
 
-type historyProps = RouteComponentProps
+interface historyProps extends RouteComponentProps {
+    category?: string,
+    query?: string,
+    algoliaSearch?: (category: string, query: string, region: string) => void
+}
+
+type comparisonProps = {
+    category: string,
+    query: string,
+    algoliaSearch: (category: string, query: string, region: string) => void
+}
+
+type propsType = historyProps & comparisonProps
 
 const RegionButton: React.FC<historyProps> = (props) => {
     const regionMap = ['愛知県名古屋市', '愛知県小牧市']
+    const { category, query, algoliaSearch } = props as propsType
 
     return (
         <div>
             {regionMap.map((region) => (
-                <button key={region} onClick={() => props.history.push('./category?region=' + region)}>{region}</button>
+                category ?
+                    <button key={region} onClick={() => algoliaSearch(category, query, region)}>{region}</button> :
+                    <button key={region} onClick={() => props.history.push('./category?region=' + region)}>{region}</button>
             ))}
         </div>
     )
+
 }
 export default withRouter<historyProps, React.FC<historyProps>>(RegionButton)
