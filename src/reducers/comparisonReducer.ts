28abeb95dc5c06ee234182e1c1a41385186191ce
sasa-kou <sasakou11428@gmail.com>
2019-10-07 18:50:54 +0900

    return するデータを場合分け

--- src/reducers/comparisonReducer.ts
+++ src/reducers/comparisonReducer.ts
@@ -6,11 +6,25 @@ const initialState: Array<TabsState> = []
 
 export const ComparisonReducer = reducerWithInitialState(initialState)
     .case(fetchSystemToComparisonCreator.started, (state) => {
-        return [...state]
+        return Array.from(state)
     })
     .case(fetchSystemToComparisonCreator.done, (state: Array<TabsState>, fetchData) => {
-        return [...state].concat(fetchData.result)
+        const fetchFlag = state.some((value: TabsState) => { return JSON.stringify(value) === JSON.stringify(fetchData.result[0]) })
+        if (fetchFlag) return state
+        else {
+            const regionFlag = state.some((value: TabsState) => { return value.region === fetchData.result[0].region })
+            if (regionFlag) {
+                for (let i = 0; i < state.length; i++) {
+                    if (state[i].region === fetchData.result[0].region) {
+                        state.splice(i, 1, fetchData.result[0])
+                    }
+                }
+                console.log('CH', state)
+                return Array.from(state)
+            }
+            else return state.concat(fetchData.result)
+        }
     })
     .case(initComparisonCreator, () => {
-        return []
+        return Array.from([])
     })
