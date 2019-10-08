ef57246153aea45532413a998310bae962f43807
watamoko <46018454+watamoko@users.noreply.github.com>
2019-10-08 16:03:32 +0900

    fix: 新制度登録でvalidationつける (#279)
    /admin/newSystem
    postにif文生やした

--- src/admin/pages/newSystem.tsx
+++ src/admin/pages/newSystem.tsx
@@ -92,7 +92,7 @@ const Input: React.FC<historyProps> = props => {
   const [selectionTargetFamily, setSelectionTargetFamily] = useState<number[]>(
     []
   );
-  const [targetAge, setTargetAge] = useState<TargetAge>(0);
+  const [targetAge, setTargetAge] = useState<TargetAge>(-1);
 
   const categoryList: Array<string> = [
     "子育て",
@@ -130,29 +130,42 @@ const Input: React.FC<historyProps> = props => {
       targetAge: targetAge
     };
     console.log(systemData);
-    const systemCollection = fireStore.collection(systemIndex);
-    systemCollection
-      .add(systemData)
-      .then(ref => {
-        console.log("Added document with ID: ", ref.id);
-        const mode = "no-cors";
-        const method = "POST";
-        const body = JSON.stringify(systemData);
-        const headers = {
-          Accept: "application/json"
-        };
-        fetch(
-          "https://script.google.com/macros/s/AKfycbz4hzx40TvDLIl4MGARBmECM1Gpp3kjb_LUEafA81O3SQ3oC2Pk/exec",
-          { mode, method, headers, body }
-        )
-          .then(res => {
-            console.log(res);
-            alert("登録が完了しました。");
-            props.history.push("/admin/");
-          })
-          .catch(err => console.error(err));
-      })
-      .catch(err => console.error(err));
+    if (systemName === "" ||
+      department === "" ||
+      location === "" ||
+      site === "" ||
+      target === "" ||
+      detail === "" ||
+      method === "" ||
+      selectionCategory.length === 0 ||
+      selectionTargetFamily.length === 0 ||
+      targetAge === -1) {
+      alert('必須項目が入力されていません')
+    } else {
+      const systemCollection = fireStore.collection(systemIndex);
+      systemCollection
+        .add(systemData)
+        .then(ref => {
+          console.log("Added document with ID: ", ref.id);
+          const mode = "no-cors";
+          const method = "POST";
+          const body = JSON.stringify(systemData);
+          const headers = {
+            Accept: "application/json"
+          };
+          fetch(
+            "https://script.google.com/macros/s/AKfycbz4hzx40TvDLIl4MGARBmECM1Gpp3kjb_LUEafA81O3SQ3oC2Pk/exec",
+            { mode, method, headers, body }
+          )
+            .then(res => {
+              console.log(res);
+              alert("登録が完了しました。");
+              props.history.push("/admin/");
+            })
+            .catch(err => console.error(err));
+        })
+        .catch(err => console.error(err));
+    }
   };
 
   const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
