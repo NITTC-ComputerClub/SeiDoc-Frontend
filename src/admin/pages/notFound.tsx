839a32b9e8ac728f1e8b27b1bb2fc4e014e18d92
Minami Hitoki <mkmokymh@gmail.com>
2019-10-08 17:22:32 +0900

    add: Not Found Image

--- src/admin/pages/notFound.tsx
+++ src/admin/pages/notFound.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Wrapper } from '../../designSystem/Page';
+import { Wrapper, Container, MainContents } from '../../designSystem/Page';
 import AdminHeader from '../components/header';
 import AdminFooter from '../../user/components/footer-pc';
 import UserHeader from '../../user/components/header'
 import UserFooter from '../../user/components/footer'
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store';
+import styled from 'styled-components';
+import setting from '../../designSystem/setting';
 
+const StyledNotFound = styled.div`
+    background-color: ${setting.White};
+    padding: 32px;
+    text-align: center;
+
+    img {
+        max-width: 480px;
+        width: 90%;
+    }
+`
 
 const NotFound: React.FC = () => {
     const user = useSelector((state: AppState) => state.userState)
@@ -15,11 +27,11 @@ const NotFound: React.FC = () => {
         return (
             <Wrapper>
                 <AdminHeader />
-                    <div>
-                        <p>404</p>
-                        <p>Not Found</p>
-                        <p>お探しのページは見つかりませんでした。</p>
-                    </div>
+                    <StyledNotFound>
+                        <MainContents>
+                            <img src="/img/NotFound.png" alt="NotFound"></img>
+                        </MainContents>
+                    </StyledNotFound>
                 <AdminFooter />
            </Wrapper> 
         )
@@ -27,11 +39,11 @@ const NotFound: React.FC = () => {
         return (
             <Wrapper>
                 <UserHeader />
-                    <div>
-                        <p>404</p>
-                        <p>Not Found</p>
-                        <p>お探しのページは見つかりませんでした。</p>
-                    </div>
+                <StyledNotFound>
+                    <MainContents>
+                        <img src="/img/NotFound.png" alt="NotFound"></img>
+                    </MainContents>
+                </StyledNotFound>
                 <UserFooter />
             </Wrapper>
         )
