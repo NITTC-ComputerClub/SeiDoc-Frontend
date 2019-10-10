2eef225324ef82d4820fce6d75dcaaf593e5614c
MinamiHitoki <mkmokymh@gmail.com>
2019-10-10 09:41:39 +0900

    add: 共通化したボタンでかんたん修正

--- src/user/pages/region.tsx
+++ src/user/pages/region.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import RegionButton from '../components/regionButton'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import { Wrapper } from '../../designSystem/Page';
+import { Wrapper, MainContents, Container } from '../../designSystem/Page';
+import styled from 'styled-components';
+import setting from '../../designSystem/setting';
 
+const Title = styled.h1`
+    font-size: ${setting.H1};
+`
 
 const Region: React.FC = () => {
     return (
         <Wrapper>
             <Header />
-            <p>地域の選択</p>
+            <Container>
+                <MainContents>
+                    <h1>地域の選択</h1>
+                </MainContents>
+            </Container>
             <RegionButton />
             <Footer />
         </Wrapper>
