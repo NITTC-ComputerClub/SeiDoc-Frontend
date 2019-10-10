c86ec60608f8566935907d97b0cfd0dc52122c8e
sasa-kou <sasakou11428@gmail.com>
2019-10-11 02:12:01 +0900

    add grayButton

--- src/user/components/regionButton.tsx
+++ src/user/components/regionButton.tsx
@@ -7,26 +7,34 @@ import Button from '../../designSystem/Button'
 interface historyProps extends RouteComponentProps {
     category?: string,
     query?: string,
+    regionList?: Array<string>
 }
 
 type comparisonProps = {
     category: string,
     query: string,
+    regionList: Array<string>
 }
 
 type propsType = historyProps & comparisonProps
 
 const RegionButton: React.FC<historyProps> = (props) => {
     const regionMap = ['愛知県名古屋市', '愛知県小牧市']
-    const { category, query } = props as propsType
+    const { category, query, regionList } = props as propsType
     const dispatch = useDispatch()
     const addComparsion = (query: string, category: string, region: string) => dispatch(fetchSystemToComparison(query, category, region))
 
+    const checkValue = (region: string) => {
+        if (regionList.includes(region))
+            return (<Button gray disabled key={region}>{region}</Button>)
+        else
+            return (<Button blue wide key={region} onClick={() => addComparsion(query, category, region)}>{region}</Button>)
+    }
     return (
         <div>
             {regionMap.map((region) => (
                 category !== undefined || query !== undefined ?
-                    <Button blue wide key={region} onClick={() => addComparsion(query, category, region)}>{region}</Button> :
+                    checkValue(region) :
                     <Button blue wide key={region} onClick={() => props.history.push('./category?region=' + region)}>{region}</Button>
             ))}
         </div>
